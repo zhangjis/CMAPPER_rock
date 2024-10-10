078b5bab065156e6e1771f364ad286c8e7fa4ee0
Yubo Su <yubo56@gmail.com>
2024-10-10 16:06:52 -0400

    cleanup setup.py

--- setup_pre_adiabat.py
+++ setup_pre_adiabat.py
@@ -1,9 +1,5 @@
-# try:
-#     from distutils.core import setup
-# except:
 from setuptools import setup
 from Cython.Build import cythonize
 import numpy
 
 setup(ext_modules = cythonize('*.pyx'),include_dirs=[numpy.get_include()])
-#setup(ext_modules = cythonize('speed_5ME_Earth_001HHe.pyx'),include_dirs=[numpy.get_include()])
